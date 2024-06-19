074d0bc9bc58c4cecf4782d0920f8255334495ac
Jacob Ebey <jacob.ebey@live.com>
2024-06-18 21:59:29 -0700

    update do

--- site/src/durable-objects.ts
+++ site/src/durable-objects.ts
@@ -1,3 +1,7 @@
 export { RateLimiter } from "./durable-objects/rate-limiter.js";
 
-export default {};
+export default {
+	fetch() {
+		return new Response("Not Found", { status: 404, statusText: "Not Found" });
+	},
+};
